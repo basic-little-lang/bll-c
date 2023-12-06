f912c533c6f829bf3f08c0362fe364743e513df8
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-06 18:55:13 +1100

    Add vector_init()

--- src/vector.c
+++ src/vector.c
@@ -1,3 +1,12 @@
+#include <stdlib.h>
 #include "include/vector.h"
 
+vector_t* vector_init() {
+    vector_t* vec = malloc(sizeof(vector_t));
+    vec->capacity = VECTOR_STARTING_CAPACITY;
+    vec->size = 0;
+    
+    vec->data = malloc(sizeof(void*) * vec->capacity);
 
+    return vec;
+}
