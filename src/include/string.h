9c09827d9269284a6ce93d92814bfe9e84a2d2ee
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-06 20:55:50 +1100

    Add string functions

--- src/include/string.h
+++ src/include/string.h
@@ -7,6 +7,12 @@
 typedef vector_t string_t;
 
 string_t* string_init();
+int string_size(const string_t* str);
+int string_capacity(const string_t* str);
+char** string_data(const string_t* str);
+char* string_get(const string_t* str, int index);
+void string_add(string_t *restrict str, char chr);
+char* string_remove(string_t* str, int index);
 void string_destory(string_t* str);
 
 #endif
--- src/string.c
+++ src/string.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "include/vector.h"
 #include "include/string.h"
 
@@ -5,6 +6,35 @@ string_t* string_init() {
     return vector_init();
 }
 
+int string_size(const string_t* str) {
+    return vector_size(str);
+}
+
+int string_capacity(const string_t* str) {
+    return vector_capacity(str);
+}
+
+char** string_data(const string_t* str) {
+    return (char**) vector_data(str);
+}
+
+char* string_get(const string_t* str, int index) {
+    return (char*) vector_get(str, index);
+}
+
+void string_add(string_t *restrict str, char chr) {
+    char* heap_char = malloc(sizeof(char));
+    *heap_char = chr;
+    vector_add(str, heap_char);
+}
+
+char* string_remove(string_t* str, int index) {
+    return (char*) vector_remove(str, index);
+}
+
 void string_destory(string_t* str) {
+    for (int i = 0; i < string_size(str); i++) {
+        free(string_get(str, i));
+    }
     vector_destroy(str);
 }
