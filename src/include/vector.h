e738a8b078ac19a6873ae9d9ca7bb15f8e7bae51
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-06 18:55:02 +1100

    Add functions to header file

--- src/include/vector.h
+++ src/include/vector.h
@@ -2,11 +2,20 @@
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
 
+#define VECTOR_STARTING_CAPACITY 4;
+
 typedef struct {
     int size;
     int capacity;
     void **data;
 } vector_t;
 
+vector_t* vector_init();
+int vector_size(const vector_t* vec);
+int vector_capacity(const vector_t* vec);
+void** vector_data(const vector_t* vec);
+void* vector_get(const vector_t* vec, int index);
+void* vector_remove(const vector_t* vec, int index);
+void vector_destroy(const vector_t* vec);
 
 #endif
