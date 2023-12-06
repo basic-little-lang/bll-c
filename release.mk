d8bf7c5e77997327fba0df062b3b59736a4e0a49
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-06 18:27:05 +1100

    Init

---
+++ release.mk
@@ -0,0 +1,29 @@
+CC = gcc
+CCFLAGS = -c -Wall --std=c99
+
+LINKER = gcc
+LINKERFLAGS = -Wall --std=c99 -o
+
+PROG = target
+
+SRCDIR = src
+BINARYDIR = bin
+
+OBJS = $(patsubst $(SRCDIR)/%.c, $(BINARYDIR)/%.o, $(wildcard $(SRCDIR)/*.c))
+
+$(BINARYDIR):
+	mkdir $(BINARYDIR)
+
+$(PROG): $(OBJS)
+	$(LINKER) $(OBJS) $(LINKERFLAGS) $(PROG)
+
+$(BINARYDIR)/%.o: $(SRCDIR)/%.c
+	$(CC) $(CCFLAGS) $< -o $@
+
+all: $(BINARYDIR) $(PROG)
+
+clean:
+	rm -rf *.o $(PROG) $(BINARYDIR)
+
+.PHONY: all clean
+.DEFAULT_GOAL = all
