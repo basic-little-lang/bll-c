c854cc662db5cccc710f7a8a68b35861303b9a85
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-06 19:48:47 +1100

    Test basic vectors

--- src/main.c
+++ src/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "include/vector.h"
 
 int main() {
-    printf("Hello world\n");
+    
+    vector_t* vec = vector_init();
+
+    for (int i = 0; i < 12; i++) {
+        int *num = malloc(sizeof(int));
+        *num = i + 1;
+        vector_add(vec, num);
+    }
+
+    for (int i = 0; i < vector_size(vec); i++) {
+        printf("%d\n", *((int*) vector_get(vec, i)));
+        free(vector_get(vec, i));
+    }
+
+    printf("%d and %d\n", vector_size(vec), vector_capacity(vec));
+
+    vector_destroy(vec);
+
 }
