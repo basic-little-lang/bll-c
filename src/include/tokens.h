8a79893fa014cf2368a4ccd8c9e918a66b2a9285
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-07 20:49:35 +1100

    Add token_type_t enum type

--- src/include/tokens.h
+++ src/include/tokens.h
@@ -2,6 +2,18 @@
 #ifndef _TOKENS_H_
 #define _TOKENS_H_
 
-
+typedef enum {
+    ADD,
+    SUBTRACT,
+    DIVIDE,
+    MULTIPLIE,
+    MODULO,
+    POWER,
+    NEGITIVE,
+    EQUAL,
+    CHAR,
+    POINT,
+    WHITESPACE,
+} token_type_t;
 
 #endif
