fb1e40e78b40094de44e6ba49dcfbc017f6ae399
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-07 16:36:55 +1100

    Remove test code from main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include "include/string.h"
 #include "include/vector.h"
-#include "include/color.h"
+#include "include/string.h"
 
 int main(int argc, char const *argv[]) {
     
@@ -11,15 +10,6 @@ int main(int argc, char const *argv[]) {
     for (int i = 0; i < argc; i++) {
         vector_add(args, string_from((int) strlen(argv[i]), argv[i]));
     }
-
-
-
-    for (int i = 0; i < vector_size(args); i++) {
-        color_print_color(vector_get(args, i), TEXT_COLOR_FOREGROUND_LIGHT_MAGENTA);
-        printf(" ");
-    }
-    printf("\n");
-
 
 
     for (int i = 0; i < vector_size(args); i++) {
