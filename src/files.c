d85d1c1007ca1a28a3e4b9e9fc36b336656bf483
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-07 19:24:14 +1100

    Add files_read_file_to_string() code

--- src/files.c
+++ src/files.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include "include/string.h"
 #include "include/files.h"
 
+string_t* files_read_file_to_string(string_t* file_name) {
 
+    FILE* file = fopen(*string_data(file_name), "r");
+    if (file == NULL) return NULL;
+
+    string_t* contents = string_init();
+
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        string_add(contents, c);
+    }
+
+    fclose(file);
+
+    return contents;
+}
