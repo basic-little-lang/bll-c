ec1c7b56daf8062d2bf3530bc3634e7300043e28
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-07 17:27:01 +1100

    Add functions to header file

--- src/include/args.h
+++ src/include/args.h
@@ -2,6 +2,14 @@
 #ifndef _ARGS_H_
 #define _ARGS_H_
 
+#include "vector.h"
+#include "string.h"
 
+typedef struct {
+    string_t* file_name;
+} args_t;
+
+args_t* args_build(const vector_t* args);
+void args_destroy(args_t* args);
 
 #endif
