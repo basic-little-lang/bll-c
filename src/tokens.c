268eb348b311e55bc7b33d913711ff691e2b6887
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 07:50:34 +1100

    Add code for token_base_init()

--- src/tokens.c
+++ src/tokens.c
@@ -1,4 +1,12 @@
 #include "include/string.h"
 #include "include/tokens.h"
+#include <stdlib.h>
 
+token_t* token_base_init(token_type_t type) {
+    token_t* token = malloc(sizeof(token_t));
 
+    token->token_type = type;
+    token->data = NULL;
+
+    return token;
+}
