f05ebf2c3601334980e355768f09994a91111be8
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 16:49:20 +1100

    Add code for token parsing

--- src/tokens.c
+++ src/tokens.c
@@ -2,6 +2,10 @@
 #include "include/string.h"
 #include "include/tokens.h"
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+const char TOKEN_COMMENT_CHAR = '#';
 
 token_t* token_base_init(token_type_t type) {
     token_t* token = malloc(sizeof(token_t));
@@ -124,7 +128,96 @@ void token_destory(token_t* token) {
 vector_t* token_tokenize_string(string_t* str) {
     vector_t* tokens = vector_init();
 
-    
+    bool skip_comment = false;
+    bool free_if_fail = false;
+
+    for (int i = 0; i < string_size(str); i++) {
+        char chr = *string_get(str, i);
+
+        if (chr == '\n') {
+            if (skip_comment) {
+                skip_comment = false;
+                continue;
+            }
+            vector_add(tokens, token_base_init(TOKEN_TYPE_WHITESPACE));
+            free_if_fail = true;
+            continue;
+        }
+
+        if (skip_comment) continue;
+        if (chr == TOKEN_COMMENT_CHAR) {
+            skip_comment = true;
+            continue;
+        }
+        if (isspace(chr)) {
+            vector_add(tokens, token_base_init(TOKEN_TYPE_WHITESPACE));
+            free_if_fail = true;
+            continue;
+        }
+
+        switch (chr) {
+            case '+':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_ADD));
+                free_if_fail = true;
+                break;
+            case '-':
+                {
+                    int next_index = i + 1;
+                    if (next_index > string_size(str) && next_index > 0) {
+                        if (free_if_fail) {
+                            for (int j = 0; j < vector_size(tokens); j++) {
+                                token_destory(vector_get(tokens, j));
+                            }
+                        }
+                        vector_destroy(tokens);
+                        return NULL;
+                    }
+
+                    char ch = *string_get(str, next_index);
+
+                    if (isspace(ch)) {
+                        vector_add(tokens, token_base_init(TOKEN_TYPE_SUBTRACT));
+                    } else {
+                        vector_add(tokens, token_base_init(TOKEN_TYPE_NEGITIVE));
+                    }
+                    free_if_fail = true;
+
+                }
+                break;
+            case '*':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_MULTIPLIE));
+                free_if_fail = true;
+                break;
+            case '/':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_DIVIDE));
+                free_if_fail = true;
+                break;
+            case '%':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_MODULO));
+                free_if_fail = true;
+                break;
+            case '^':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_POWER));
+                free_if_fail = true;
+                break;
+            case '.':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_POINT));
+                free_if_fail = true;
+                break;
+            case '=':
+                vector_add(tokens, token_base_init(TOKEN_TYPE_EQUAL));
+                free_if_fail = true;
+                break;
+            default:
+                if (!isdigit(chr) && !isalpha(chr)) {
+                    continue;
+                }
+                vector_add(tokens, token_char_init(chr));
+                free_if_fail = true;
+                break;
+        }
+
+    }
 
     return tokens;
 }
