55137e23d3ad49021a9a6fa2775dedf76df8fad3
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 20:22:59 +1100

    Add code for parser_token_var_init()

--- src/parser.c
+++ src/parser.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "include/string.h"
 #include "include/parser.h"
 
 parser_token_t* parser_token_base_init(parser_token_type_t token_type) {
@@ -16,6 +17,21 @@ parser_token_t* parser_token_number_init(double data) {
     token->token_type = PARSER_TOKEN_TYPE_NUMBER;
     token->data = malloc(sizeof(double));
     *((double*) token->data) = data;
+
+    return token;
+}
+
+parser_token_t* parser_token_var_init(const string_t* data) {
+    parser_token_t* token = malloc(sizeof(parser_token_t));
+
+    token->token_type = PARSER_TOKEN_TYPE_VAR;
+
+    string_t* str = string_init();
+    for (int i = 0; i < string_size(data); i++) {
+        string_add(str, *string_get(data, i));
+    }
+
+    token->data = str;
 
     return token;
 }
