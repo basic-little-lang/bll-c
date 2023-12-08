e40188b411183acaf4728d68b19c5a9b8060ad60
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 22:43:35 +1100

    Add code to parser_convert()

--- src/parser.c
+++ src/parser.c
@@ -1,10 +1,23 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include "include/vector.h"
 #include "include/string.h"
 #include "include/tokens.h"
 #include "include/parser.h"
+
+parser_token_t* parser_string_to_keyword(const string_t* str) {
+    if (strcmp(*string_data(str), "print") == 0) {
+        return parser_token_base_init(PARSER_TOKEN_TYPE_PRINT);
+    }
+    if (strcmp(*string_data(str), "copy") == 0) {
+        return parser_token_base_init(PARSER_TOKEN_TYPE_COPY);
+    }
+
+    return NULL;
+}
 
 parser_token_t* parser_token_base_init(parser_token_type_t token_type) {
     parser_token_t* token = malloc(sizeof(parser_token_t));
@@ -147,7 +160,113 @@ vector_t* parser_convert(const vector_t* direct_tokens) {
     for (int i = 0; i < vector_size(direct_tokens); i++) {
         token_t* token = vector_get(direct_tokens, i);
 
+        switch (token_type(token)) {
+            case TOKEN_TYPE_CHAR:
+                vector_add(buffer, token);
+
+                {
+                    string_t* string = string_init();
+
+                    for (int j = 0; j < vector_size(buffer); j++) {
+                        switch (token_type(vector_get(buffer, j))) {
+                            case TOKEN_TYPE_CHAR:
+                                string_add(string, *((char*) token_data(vector_get(buffer, j))));
+                                break;
+                            case TOKEN_TYPE_POINT:
+                                string_add(string, '.');
+                                break;
+                            case TOKEN_TYPE_NEGITIVE:
+                                string_add(string, '-');
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    bool next_is_whitespace = true;
+                    int next_index = i + 1;
+                    if (next_index < vector_size(direct_tokens)) next_is_whitespace = (token_type(vector_get(direct_tokens, next_index)) == TOKEN_TYPE_WHITESPACE);
+
+                    if (isdigit(*((char*) token_data(vector_get(direct_tokens, i)))) && next_is_whitespace) {
+                        bool is_negitive = false;
+                        for (int j = 0; j < string_size(string); j++) {
+                            if (*string_get(string, j) == '-') {
+                                is_negitive = true;
+                                break;
+                            }
+                        }
+                        string_t* str = string_init();
+                        for (int j = 0; j < string_size(string); j++) {
+                            char chr = *string_get(string, j);
+                            if (chr == '-') continue;
+                            string_add(str, chr);
+                        }
         
+                        char num_str[string_size(str) + 1];
+                        for (int j = 0; j < string_size(str); j++) {
+                            num_str[j] = *string_get(str, j);
+                        }
+                        num_str[string_size(str)] = 0;
+                        double number = atof(num_str);
+
+                        if (is_negitive) number *= -1.0;
+
+                        vector_add(parsered_tokens, parser_token_number_init(number));
+
+                        string_destory(str);
+
+                        vector_destroy(buffer);
+                        buffer = vector_init();
+                        string_destory(string);
+
+                    } else if (next_is_whitespace) {
+                        parser_token_t* keyword_token = parser_string_to_keyword(string);
+                        if (keyword_token == NULL) {
+                            vector_add(parsered_tokens, parser_token_var_init(string));
+                        } else {
+                            vector_add(parsered_tokens, keyword_token);
+                        }
+
+                        vector_destroy(buffer);
+                        buffer = vector_init();
+                    } else {
+                        string_destory(string);
+                    }
+                
+
+                }
+
+                break;
+            case TOKEN_TYPE_ADD:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_ADD));
+                break;
+            case TOKEN_TYPE_SUBTRACT:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_SUBTRACT));
+                break;
+            case TOKEN_TYPE_MULTIPLIE:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_MULTIPLIE));
+                break;
+            case TOKEN_TYPE_DIVIDE:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_DIVIDE));
+                break;
+            case TOKEN_TYPE_MODULO:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_MODULO));
+                break;
+            case TOKEN_TYPE_POWER:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_POWER));
+                break;
+            case TOKEN_TYPE_POINT:
+                vector_add(buffer, token);
+                break;
+            case TOKEN_TYPE_EQUAL:
+                vector_add(parsered_tokens, parser_token_base_init(PARSER_TOKEN_TYPE_EQUAL));
+                break;
+            case TOKEN_TYPE_NEGITIVE:
+                vector_add(buffer, token);
+                break;
+            case TOKEN_TYPE_WHITESPACE:
+                break;
+        }
 
     }
 
