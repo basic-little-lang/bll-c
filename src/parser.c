27c29dba1dbdcafd5c687ac0ebf937fff05dcf8c
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 20:12:12 +1100

    Add code for parser_token_number_init()

--- src/parser.c
+++ src/parser.c
@@ -9,3 +9,13 @@ parser_token_t* parser_token_base_init(parser_token_type_t token_type) {
 
     return token;
 }
+
+parser_token_t* parser_token_number_init(double data) {
+    parser_token_t* token = malloc(sizeof(parser_token_t));
+
+    token->token_type = PARSER_TOKEN_NUMBER;
+    token->data = malloc(sizeof(double));
+    *((double*) token->data) = data;
+
+    return token;
+}
