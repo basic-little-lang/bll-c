8ba32cd92fa8a8fa1af294d0e17fe5f87b6ccf59
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 19:56:09 +1100

    Add code for parser_token_base_init()

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
 #include "include/parser.h"
 
+parser_token_t* parser_token_base_init(parser_token_type_t token_type) {
+    parser_token_t* token = malloc(sizeof(parser_token_t));
 
+    token->token_type = token_type;
+    token->data = NULL;
+
+    return token;
+}
