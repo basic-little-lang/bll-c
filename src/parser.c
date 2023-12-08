88c29163df18fb3ec1e61beccda33b221a276c10
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-08 20:12:51 +1100

    Bug fix

--- src/parser.c
+++ src/parser.c
@@ -13,7 +13,7 @@ parser_token_t* parser_token_base_init(parser_token_type_t token_type) {
 parser_token_t* parser_token_number_init(double data) {
     parser_token_t* token = malloc(sizeof(parser_token_t));
 
-    token->token_type = PARSER_TOKEN_NUMBER;
+    token->token_type = PARSER_TOKEN_TYPE_NUMBER;
     token->data = malloc(sizeof(double));
     *((double*) token->data) = data;
 
