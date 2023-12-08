a06607ce0bcee07f7769ed7c2a89e64984a3c18f
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-09 10:00:58 +1100

    Add basic execute with bug fixes

--- src/execute.c
+++ src/execute.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 #include "include/vector.h"
 #include "include/string.h"
+#include "include/parser.h"
 #include "include/execute.h"
 
 typedef struct {
@@ -20,7 +23,7 @@ vars_t* vars_init() {
 }
 
 void vars_insert(vars_t* vars, const string_t* key_str, double val) {
-    char key[string_size(key_str) + 1];
+    char* key = malloc(sizeof(char) * (string_size(key_str) + 1));
     for (int i = 0; i < string_size(key_str); i++) {
         key[i] = *string_get(key_str, i);
     }
@@ -60,6 +63,9 @@ void vars_update_or_insert(vars_t* vars, const string_t* key_str, double val) {
 }
 
 void vars_destory(vars_t* vars) {
+    for (int i = 0; i < vector_size(vars->keys); i++) {
+        free(vector_get(vars->keys, i));
+    }
     for (int i = 0; i < vector_size(vars->vals); i++) {
         free(vector_get(vars->vals, i));
     }
@@ -69,5 +75,425 @@ void vars_destory(vars_t* vars) {
 }
 
 bool execute_execute(const vector_t* tokens) {
+    vars_t* vars = vars_init();
+    double last_val = 0;
 
+    for (int i = 0; i < vector_size(tokens); i++) {
+        parser_token_t* token = vector_get(tokens, i);
+
+        parser_token_t* last_token;
+        parser_token_t* next_token;
+
+        int last_index = i - 1;
+        int next_index = i + 1;
+
+        if (last_index < 0) {
+            last_token = NULL;
+        } else {
+            last_token = vector_get(tokens, last_index);
+        }
+
+        if (!(next_index < vector_size(tokens))) {
+            next_token = NULL;
+        } else {
+            next_token = vector_get(tokens, next_index);
+        }
+
+        switch (parser_token_type(token)) {
+            case PARSER_TOKEN_TYPE_ADD:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = last_v + next_v;
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_SUBTRACT:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = last_v - next_v;
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_MULTIPLIE:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = last_v * next_v;
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_DIVIDE:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = last_v / next_v;
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_MODULO:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = (int) last_v % (int) next_v;
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_POWER:
+                {
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    double last_v;
+                    double next_v;
+
+                    switch (parser_token_type(last_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            last_v = *((double*) parser_token_data(last_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(last_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                last_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            next_v = *((double*) parser_token_data(next_token));
+                            break;
+                        case PARSER_TOKEN_TYPE_VAR:
+                            {
+                                double val;
+                                double* p = vars_get(vars, ((string_t*) parser_token_data(next_token)));
+                                if (p != NULL) {
+                                    val = *p;
+                                } else {
+                                    val = 0;
+                                }
+                                next_v = val;
+                            }
+                            break;
+                        default:  
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                    last_val = pow(last_v, next_v);
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_EQUAL:
+                {
+
+                    if (last_token == NULL || next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                    switch (parser_token_type(next_token)) {
+                        case PARSER_TOKEN_TYPE_VAR:
+                            vars_update_or_insert(vars, parser_token_data(next_token), last_val);
+                            break;
+                        case PARSER_TOKEN_TYPE_NUMBER:
+                            switch (parser_token_type(last_token)) {
+                                case PARSER_TOKEN_TYPE_VAR:
+                                    vars_update_or_insert(vars, parser_token_data(last_token), *((double*) parser_token_data(next_token)));
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        default:
+                            vars_destory(vars);
+                            return false;
+                    }
+
+                }
+                break;
+            case PARSER_TOKEN_TYPE_PRINT:
+                {
+                    if (next_token == NULL) {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+
+                    if (parser_token_type(next_token) == PARSER_TOKEN_TYPE_VAR) {
+                        string_t* key = parser_token_data(next_token);
+                        string_print(key);
+                        double* p = vars_get(vars, key);
+                        if (p == NULL) {
+                            printf(" = 0\n");
+                        } else {
+                            printf(" = %lf\n", *p);
+                        }
+                    } else {
+                        vars_destory(vars);
+                        return false;
+                    }
+
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    vars_destory(vars);
+    return true;
 }
